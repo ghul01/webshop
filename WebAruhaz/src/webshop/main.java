9a412b98e6b11374ec3b0bb2530d2cd6c11d9a01
Attila <ghul0111@gmail.com>
2017-04-20 15:33:06 +0200

    init git gud!

---
+++ WebAruhaz/src/webshop/main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package webshop;
+
+/**
+ * @author Attila
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
