c0660f899fc1be006af4b6fc9cc40646a775c650
Attila <Attila@DESKTOP-GQ6VHUV>
2017-04-20 16:40:24 +0200

    Init main v0.1

--- WebAruhaz/src/webshop/main.java
+++ WebAruhaz/src/webshop/main.java
@@ -9,12 +9,26 @@ package webshop;
  */
 public class main {
 
+	private static String username;
+	private static String password;
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		LogIn();
+		StartApp(username,password);
+	}
 
+	private static void StartApp(String username2, String password2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void LogIn() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
