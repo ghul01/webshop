f2c6cf9f361ec3e3498246d9a7c389e9dc75a9fd
Attila <Attila@DESKTOP-GQ6VHUV>
2017-04-20 15:56:33 +0200

    init controller v0.1

---
+++ WebAruhaz/src/controller/WebShopController.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package controller;
+
+/**
+ * @author Attila
+ *
+ */
+/**
+ * Ez az osztály vezérli az egész programot, valamint a view és model csomagokat
+ * köti össze. Itt található az üzleti logika (business logic) is.
+ */
+public class WebShopController {
+
+}
