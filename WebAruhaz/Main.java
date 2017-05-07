a01587a52e5ee19aed021a2d4f3458ac96efe2dc
NorbertTG <ollainorbert10@gmail.com>
2017-05-07 14:46:33 +0200

    Add files via upload

---
+++ WebAruhaz/Main.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package webshop;
+
+import controller.WebShopController;
+
+/**
+ * @author Attila
+ * kappa Norbert w
+ *ffff
+ */
+public class Main {
+
+	private static String username;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StartApp();
+	}
+
+	private static void StartApp() {
+		WebShopController controller = new WebShopController();
+		
+		controller.startDesktop();
+		
+	}
+	/**
+	 * @return the username
+	 */
+	public static String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public static void setUsername(String newUsername) {
+		username = newUsername;
+	}
+}
